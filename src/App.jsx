b9a8349c1fca6525b19a7b8438033dc50a3b1895
jeet4747 <mrjeet088@gmail.com>
2026-02-18 13:27:51 +0530

    name changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ export default function App() {
         <section id="contact"><ContactForm /></section>
         <Footer />
         <Enroll />
-        <PaymentPage />
+        <paymentpage />
       </div>
     </EnrollProvider>
   )
